2fe2081bd94eb6a96aaf6c7e615c5229a66bea9c
keshav bathla <Keshav_Bathla@bitbucket.org>
2022-02-20 15:48:40 +0530

    Add media files settings

--- gocareers/gocareers/urls.py
+++ gocareers/gocareers/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    path, include
+)
+from django.conf.urls.static import static
+
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('job/', include('job.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Go Careers Admin Panel'
+admin.site.site_title = 'Go Careers'
